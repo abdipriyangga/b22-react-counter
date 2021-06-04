8cdfc987dc23d3eb4b5ce1111c2977fce5dde90e
abdipriyangga <mabdipme@gmail.com>
2021-06-04 16:33:05 +0700

    first commit, this repo about simple counter with react js

---
+++ src/components/Counter.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+class Counter extends React.Component {
+    state = {
+        number: 0,
+        error: ''
+    }
+    handleCounterChangeMinus = () => {
+        if(this.state.number <= 0) {
+            window.alert("Number must be greater than 0")
+        }
+        else{
+            this.setState({number: this.state.number - 1})
+        }
+        
+    }
+    handleCounterChangePlus = () => {
+        if (this.state.number >= 10) {
+            this.setState({ error: "Nummber must less than 10" })
+        }
+        else {
+            this.setState({number: this.state.number + 1})
+        }
+    }
+    render() {
+        return(
+            <>
+                <div className="container">
+                    <h1>{this.props.titleName}</h1>
+                    <button onClick={this.handleCounterChangeMinus}>-</button>
+                    <span>{this.state.number}</span>
+                    <button onClick={this.handleCounterChangePlus}>+</button>
+                    <div><p>{this.state.error}</p></div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Counter 
